f062def9f2774c865c9958fe39b79856b32b63c2
Jedidiah Onotu <jedidiahonotu@gmail.com>
2025-05-18 21:43:47 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the text content:
    
        Datably JO
    
    Relevant files:
    
        - https://6000-firebase-studio-1747439173448.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_580d8e24._.js
        - https://6000-firebase-studio-1747439173448.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747439173448.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    add a little margin to the sides

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import { Logo } from "@/components/layout/logo";
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center justify-between">
+      <div className="container px-6 flex h-16 max-w-screen-2xl items-center justify-between">
         <Logo />
         <UserNav />
       </div>
